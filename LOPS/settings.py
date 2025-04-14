49d4ff4d73892683c45d6e4dfffbd6cbc8da5b92
numan <numanaljilani001.com>
2025-04-14 12:22:03 +0530

    update

--- LOPS/settings.py
+++ LOPS/settings.py
@@ -140,7 +140,8 @@ CORS_ALLOW_METHODS = [
 ]
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    'http://192.168.21.213:3000'
+    'http://192.168.21.213:3000',
+    "http://13.61.184.90:8000"
 ]
 
 # Default primary key field type
